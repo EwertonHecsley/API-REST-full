01d6c7546fc0d69ccb89a131187fa9090b4c2e6a
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-07-31 22:21:20 -0300

    Finalizando controlodor de cadastro de usuários

--- src/controllers/controladores.js
+++ src/controllers/controladores.js
@@ -1,5 +1,25 @@
-const cadastrarUsuario = (req, res) => { };
+const bcrypt = require('bcrypt');
+const pool = require('../conexaoBD');
 
+const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+    try {
+        const usuario = await pool.query(`
+        INSERT INTO usuarios(nome,email,senha)
+        VALUES($1,$2,$3) RETURNING id,nome,email
+        `, [nome, email, senhaCriptografada]);
+
+        //const { senha: senhaOculta, ...usuarioFormatado } = usuario.rows[0]; // esta linha de cod para ocultar email, caso traga * no BD
+
+        return res.status(201).json(usuario.rows[0]);
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: error.message });
+    };
+};
 
 module.exports = {
     cadastrarUsuario
